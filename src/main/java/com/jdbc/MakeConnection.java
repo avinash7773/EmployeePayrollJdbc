1b200d503b192b346e65d0ea05ebe8adcfbe23e0
avinash7773 <javinash228@gmail.com>
2021-05-19 23:30:22 +0530

    add to make connection

--- src/main/java/com/jdbc/MakeConnection.java
+++ src/main/java/com/jdbc/MakeConnection.java
@@ -3,6 +3,7 @@ package com.jdbc;
 import java.sql.Connection;
 import java.sql.Driver;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.Enumeration;
 
 public class MakeConnection {
@@ -10,7 +11,6 @@ public class MakeConnection {
         String jdbcUrl = "jdbc:mysql://localhost:3306/employee_payroll_service?SSL=false";
         String userName = "root";
         String password = "Aiv@77094";
-        Connection connection;
 
         try {
             Class.forName("com.mysql.jdbc.Driver");
@@ -19,6 +19,15 @@ public class MakeConnection {
             throw new IllegalStateException("can not find driver in the class path");
         }
         listDrivers();
+        Connection connection;
+
+        try {
+            Connection connection1 = DriverManager.getConnection(jdbcUrl,userName,password);
+            System.out.println("Connection SuccessFull!!!");
+        } catch (SQLException throwable) {
+            throwable.printStackTrace();
+        }
+
     }
 
     /** listDrivers method is used for find list of driver
